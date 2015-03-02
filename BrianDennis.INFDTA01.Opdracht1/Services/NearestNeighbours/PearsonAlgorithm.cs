72cba04750d894baaa782d44608b34469d9eb6f3
Dennis v/d Wielen <dennis.wielen@gmail.com>
2015-03-02 11:02:35 +0100

    Pearson Algorithm Fix. Check articles

--- BrianDennis.INFDTA01.Opdracht1/Services/NearestNeighbours/PearsonAlgorithm.cs
+++ BrianDennis.INFDTA01.Opdracht1/Services/NearestNeighbours/PearsonAlgorithm.cs
@@ -20,22 +20,24 @@ namespace BrianDennis.INFDTA01.Opdracht1.Services.NearestNeighbours
             foreach (KeyValuePair<int, Dictionary<int, float>> otherUser in dataSet)
             {
                 if (otherUser.Key == targetUserId) continue;
-                /*
-                if (otherUser.Value.Count != targetUser.Count)
-                    continue;*/
 
-                float avgOther = otherUser.Value.Values.Average();
+                targetUser = targetUser.Where(m => otherUser.Value.ContainsKey(m.Key)).Select(m=>m).ToDictionary(m=>m.Key, m=>m.Value);
+
+                Dictionary<int, float> newOther = otherUser.Value.Where(m => targetUser.ContainsKey(m.Key)).Select(m => m).ToDictionary(m => m.Key, m => m.Value);
+
+                avgTarget = targetUser.Values.Average();
+                float avgOther = newOther.Values.Average();
 
                 float total1 =
-                    otherUser.Value.Zip(targetUser,
+                    newOther.Zip(targetUser,
                         (other1, target1) => (other1.Value - avgTarget)*(target1.Value - avgOther)).Sum();
 
-                double subTotal1 = Math.Sqrt(otherUser.Value.Values.Sum(x => Math.Pow((x - avgTarget), 2)));
+                double subTotal1 = Math.Sqrt(newOther.Values.Sum(x => Math.Pow((x - avgTarget), 2)));
                 double subTotal2 = Math.Sqrt(targetUser.Values.Sum(x => Math.Pow((x - avgOther), 2)));
 
                 double result = total1/(subTotal1*subTotal2);
-/*
-                ResultAdd(resultList, AlgorithmResultListItem.Build(new Tuple<int, int, double, int>(targetUserId, otherUser.Key, result, 0)), result);
+                
+                /*ResultAdd(resultList, AlgorithmResultListItem.Build(new Tuple<int, int, double, int>(targetUserId, otherUser.Key, result, 0)), result);
                 */resultList.Add(AlgorithmResultListItem.Build(new Tuple<int, int, double, int>(targetUserId, otherUser.Key, result, 0)));
             }
 
