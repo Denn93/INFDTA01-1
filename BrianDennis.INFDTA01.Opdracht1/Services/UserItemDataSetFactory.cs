0a99f38d728fc26b809b3e93fb0029c1e0ee85c7
Dennis v/d Wielen <dennis.wielen@gmail.com>
2015-03-10 13:18:26 +0100

    Commented UserItemDataset Factory

--- BrianDennis.INFDTA01.Opdracht1/Services/UserItemDataSetFactory.cs
+++ BrianDennis.INFDTA01.Opdracht1/Services/UserItemDataSetFactory.cs
@@ -8,6 +8,9 @@ namespace BrianDennis.INFDTA01.Opdracht1.Services
 {
     public static class UserItemDataSetFactory
     {
+        /// <summary>
+        /// Enum available datasets
+        /// </summary>
         public enum DataSets
         {
             UserItem,
@@ -16,10 +19,13 @@ namespace BrianDennis.INFDTA01.Opdracht1.Services
             NotFound
         }
 
-        public static SortedDictionary<int, Dictionary<int, float>> UserItemDataSet { get; set; }
-        public static SortedDictionary<int, Dictionary<int, float>> UserItemEditedDataSet { get; set; }
-        public static SortedDictionary<int, Dictionary<int, float>> MovieLensDataSet { get; set; }
-   
+        #region Factory methods
+
+        /// <summary>
+        /// Factory method creating the Dataset
+        /// </summary>
+        /// <param name="dataSet">DataSet enum item</param>
+        /// <returns>Created Dataset</returns>
         public static SortedDictionary<int, Dictionary<int, float>> Build(DataSets dataSet)
         {
             switch (dataSet)
@@ -39,6 +45,16 @@ namespace BrianDennis.INFDTA01.Opdracht1.Services
             return null;
         }
 
+        #endregion
+
+        #region Private Helper methods
+
+        /// <summary>
+        /// This private methods loads a Dataset based on filepath and added split character
+        /// </summary>
+        /// <param name="filePath">File path to Dataset</param>
+        /// <param name="split">Split character</param>
+        /// <returns>Loaded dataset into Dictionary<int, Dictionary<int, float>></returns>
         private static SortedDictionary<int, Dictionary<int, float>> LoadingConstruct(string filePath, char split)
         {
             SortedDictionary<int, Dictionary<int, float>> dataSet = new SortedDictionary<int, Dictionary<int, float>>();
@@ -56,6 +72,12 @@ namespace BrianDennis.INFDTA01.Opdracht1.Services
             return dataSet;
         }
 
+        /// <summary>
+        /// This private method processes the line of the loaded Dataset.
+        /// </summary>
+        /// <param name="line">Input line</param>
+        /// <param name="split">Split line parameter</param>
+        /// <param name="dataSet">Resulting dataset</param>
         private static void ProcessConstruct(string line, char split, IDictionary<int, Dictionary<int, float>> dataSet)
         {
             string[] row = line.Split(split);
@@ -76,6 +98,15 @@ namespace BrianDennis.INFDTA01.Opdracht1.Services
             }    
         }
 
+        #endregion
+
+        #region Public Helper Methods
+
+        /// <summary>
+        /// This method gets the Dataset by String value of the Dataset enum item
+        /// </summary>
+        /// <param name="value">DataSet value String</param>
+        /// <returns>Enum DataSets</returns>
         public static DataSets GetDatasetByString(string value)
         {
             foreach (DataSets dataSet in Enum.GetValues(typeof(DataSets)).Cast<DataSets>().Where(dataSet => dataSet.ToString().ToLower().Equals(value.ToLower())))
@@ -83,5 +114,16 @@ namespace BrianDennis.INFDTA01.Opdracht1.Services
 
             return DataSets.NotFound;
         }
+
+        #endregion
+
+        #region Properties
+
+        public static SortedDictionary<int, Dictionary<int, float>> UserItemDataSet { get; set; }
+        public static SortedDictionary<int, Dictionary<int, float>> UserItemEditedDataSet { get; set; }
+        public static SortedDictionary<int, Dictionary<int, float>> MovieLensDataSet { get; set; }
+
+        #endregion
+
     }
 }
