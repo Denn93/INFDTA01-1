74735d6b16005818ba183d201b27496a85fc5146
Dennis v/d Wielen <dennis.wielen@gmail.com>
2015-03-10 13:37:09 +0100

    Added Userpreference.cs

---
+++ BrianDennis.INFDTA01.Opdracht1/Models/UserPreference.cs
@@ -0,0 +1,10 @@
+﻿namespace BrianDennis.INFDTA01.Opdracht1.Models
+{
+    public class UserPreference 
+    {
+        public int MovieId { get; set; }
+        public string Description { get; set; }
+        public int  Rating { get; set; }
+        public string Genre { get; set; }
+    }
+}
